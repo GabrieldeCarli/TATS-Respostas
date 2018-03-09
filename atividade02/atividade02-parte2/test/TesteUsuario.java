8b03fa716bdb9e5c5e54013ce54216683d3007c0
gabriel <gabrielcarli@alunos.utfpr.edu.br>
2018-03-09 12:58:28 -0300

    Commit2

--- atividade02/atividade02-parte2/test/TesteUsuario.java
+++ atividade02/atividade02-parte2/test/TesteUsuario.java
@@ -10,35 +10,44 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import atividade02.parte2.*;
 
 /**
  *
  * @author Aluno
  */
 public class TesteUsuario {
-    
+
+    SenhaValidator senhaValidator;
+    Usuario usuario;
+    UsuarioDAO usuarioDAO;
+    UsuarioValidator usuarioValidator;
+
     public TesteUsuario() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void Teste01() throws Exception {
+        usuario.setNome("Gabriel de Carli");
+        usuario.setSenha("1234");
+        usuario.setSenhaConfirmada("12345");
+        assertEquals (, "senhas diferente");         
+    }
 }
