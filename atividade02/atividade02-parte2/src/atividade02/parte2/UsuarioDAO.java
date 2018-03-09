44561a66f338157e49efca2eab65e70605b2cc71
gabriel <gabrielcarli@alunos.utfpr.edu.br>
2018-03-09 12:36:10 -0300

    Commit1

---
+++ atividade02/atividade02-parte2/src/atividade02/parte2/UsuarioDAO.java
@@ -0,0 +1,13 @@
+package atividade02.parte2;
+
+
+import atividade02.parte2.Usuario;
+
+/**
+ *
+ * @author Aluno
+ */
+public interface UsuarioDAO {
+        public Usuario 
+                 getByName(String nomeDoUsuario);
+}
