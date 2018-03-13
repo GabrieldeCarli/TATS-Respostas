a5ac9ed012b36effefae77fcadc743724e281144
GabrieldeCarli <gabrielcarli@alunos.utfpr.edu.br>
2018-03-13 16:35:36 -0300

    Commit1303

--- atividade02/atividade02-parte2/test/TesteUsuario.java
+++ atividade02/atividade02-parte2/test/TesteUsuario.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -11,11 +7,15 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
-import atividade02.parte2.*;
+import atividade02.parte2.SenhaValidator;
+import atividade02.parte2.Usuario;
+import atividade02.parte2.UsuarioDAO;
+import atividade02.parte2.UsuarioValidator;
+
 
 /**
  *
- * @author Aluno
+ * @author Gabriel de Carli
  */
 public class TesteUsuario {
 
@@ -36,7 +36,13 @@ public class TesteUsuario {
     }
 
     @Before
+    
     public void setUp() {
+         
+        usuarioDAO = mock(UsuarioDAO.class);
+        senhaValidator = mock(SenhaValidator.class);
+        usuario = new Usuario();
+        usuarioValidator = new UsuarioValidator(usuarioDAO);
     }
 
     @After
@@ -44,10 +50,51 @@ public class TesteUsuario {
     }
 
     @Test
-    public void Teste01() throws Exception {
-        usuario.setNome("Gabriel de Carli");
-        usuario.setSenha("1234");
-        usuario.setSenhaConfirmada("12345");
-        assertEquals (, "senhas diferente");         
+    public void SenhasDiferentes() throws Exception {
+        
+        usuario.setNome("João Costa");
+        usuario.setSenha("123");
+        usuario.setSenhaConfirmada("321");
+
+        when(usuarioDAO.getByName("João Costa")).thenReturn(usuario);
+        when(senhaValidator.verificar("123")).thenReturn(true);
+        usuarioValidator.setSenhaValidator(senhaValidator);
+        assertTrue(usuarioValidator.ehUsuarioValido(usuario));
     }
+    /* Só esta retornando Usuario ja existe
+    @Test
+    public void SenhasInvalidas() throws Exception {
+        
+        usuario.setNome("João Costa");
+        usuario.setSenha("1234");
+        usuario.setSenhaConfirmada("1234");
+
+        when(usuarioDAO.getByName("João Costa")).thenReturn(usuario);
+        when(senhaValidator.verificar("1234")).thenReturn(false);
+        usuarioValidator.setSenhaValidator(senhaValidator);
+        usuarioValidator.ehUsuarioValido(usuario);
+    }
+    */
+    @Test
+    public void NomeCurto() throws Exception {
+        
+        usuario.setNome("João");
+        usuario.setSenha("123");
+        usuario.setSenhaConfirmada("123");
+        when(senhaValidator.verificar("123")).thenReturn(true);
+        when(usuarioDAO.getByName("João")).thenReturn(usuario);
+        assertTrue(usuarioValidator.ehUsuarioValido(usuario));
+    }
+    
+    @Test
+    public void UsuarioExiste() throws Exception {
+        
+        usuario.setNome("João Costa");
+        usuario.setSenha("12345");
+        usuario.setSenhaConfirmada("12345");
+        when(usuarioDAO.getByName("João Costa")).thenReturn(usuario);
+        usuarioValidator.setSenhaValidator(senhaValidator);
+        usuarioValidator.ehUsuarioValido(usuario);
+    }
+    
 }
