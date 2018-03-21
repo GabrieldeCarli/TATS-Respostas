81aff1279fbbf29a2e684b18613ede07e06e7286
GabrieldeCarli <gabrielcarli@alunos.utfpr.edu.br>
2018-03-21 11:23:56 -0300

    Commit1
    Teste Falhou

---
+++ atividade04/atividade04_CalcSalario/test/CalcSalarioTest.java
@@ -0,0 +1,43 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author gabriel de carli
+ */
+
+public class CalcSalarioTest {
+    
+    public CalcSalarioTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void CalcSalario(){
+    
+        Funcionario func = new Funcionario("Gabriel", "gabriel@email.com",
+                3000.0, "DESENVOLVEDOR" );
+        assertEquals(4000.00, func.salFinal(), 0000.1);
+        
+        
+    }
+}
